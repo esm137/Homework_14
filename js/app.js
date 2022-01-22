74d6e9c9bcf06575f8d0855caa3ff6d9eacc31d3
esm137 <90230252+esm137@users.noreply.github.com>
2022-01-22 00:02:44 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,220 @@
+let data = d3.json("Samples.json");
+
+
+
+
+
+
+
+
+// function that populates the metadata
+function demoInfo(sample)
+{
+  
+ 
+ //use d3.json to get data
+ d3.json("samples.json").then((data)=>
+ {
+    // grab all of the metadata
+    let metaData =data.metadata;
+    
+
+    // filter based on the value of the sample
+    let result =metaData.filter(sampleResult=> sampleResult.id == sample);
+
+    
+
+    //access index 0 from the array
+    let resultData = result[0];
+    
+
+    //clear the metadata out
+    d3.select("#sample-metadata").html(``);
+
+    //use Object.entries to get value key pairs
+    Object.entries(resultData).forEach(([key, value])=>
+    {
+        //add to the sample data/ demographics section
+        d3.select("#sample-metadata")
+            .append("h5").text(`${key}: ${value}`);
+    }
+    );
+ });
+
+}
+// function that builds the graphs
+function buildBarChart(sample)
+{
+   
+
+     //use d3.json to get data
+ d3.json("samples.json").then((data)=>
+ {
+    // grab all of the sampledata
+    let sampleData =data.samples;
+    
+
+    // filter based on the value of the sample
+    let result =sampleData.filter(sampleResult=> sampleResult.id == sample);
+
+    
+
+    //access index 0 from the array
+    let resultData = result[0];
+    
+    
+    // get the otu_ids
+    let otu_ids =resultData.otu_ids;
+    let otu_labels=resultData.otu_labels;
+
+    //get sample values
+
+    let sample_values= resultData.sample_values;
+
+    
+
+    // build the bar chart
+
+    // get the yTicks
+    let yTicks= otu_ids.slice(0,10).map(id=> `OTU ${id}`);
+    let xValues= sample_values.slice(0,10);
+    let textLabels = otu_labels.slice(0,10);
+
+    let barChart=
+    {
+        y: yTicks.reverse(),
+        x: xValues.reverse(),
+        text: textLabels,
+        type: "bar",
+        orientation:"h"
+    };
+    let layout = 
+    {
+        title:"Top 10 Belly Button Bacteria"
+    };
+
+    Plotly.newPlot("bar",[barChart],layout);
+ });
+}
+//function that builds the bubble chart
+function buildBubbleChart(sample)
+{
+
+     //use d3.json to get data
+     d3.json("samples.json").then((data)=>
+     {
+        // grab all of the sampledata
+        let sampleData =data.samples;
+        
+    
+        // filter based on the value of the sample
+        let result =sampleData.filter(sampleResult=> sampleResult.id == sample);
+    
+        
+    
+        //access index 0 from the array
+        let resultData = result[0];
+        
+        
+        // get the otu_ids
+        let otu_ids =resultData.otu_ids;
+        let otu_labels=resultData.otu_labels;
+    
+        //get sample values
+    
+        let sample_values= resultData.sample_values;
+    
+        
+    
+        // build the bar chart
+    
+        
+    
+        let bubbleChart=
+        {
+            y: sample_values,
+            x: otu_ids,
+            text: otu_labels,
+            mode: "markers",
+            marker:
+            {
+                size:sample_values,
+                color: otu_ids,
+                colorscale:"Earth"
+            }
+            
+        };
+        let layout = 
+        {
+            title:"Bacteria Cultures for Sample",
+            hovermode: "closest",
+            xaxis:{title : "OTU ID"}
+        };
+    
+        Plotly.newPlot("bubble",[bubbleChart],layout);
+     });
+}
+
+// function that initializes the dashboard
+function initalize()
+{
+    
+
+    // access the dropdown selector from index.html
+    var select = d3.select("#selDataset");
+
+    // use d3.json inorder to get the data
+d3.json("samples.json").then((data)=>
+{ 
+    // make array of names
+    let sampleNames = data.names; 
+    
+
+    // foreach for each sample in selector
+    sampleNames.forEach(sample => {
+        select.append("option").text(sample).property("value",sample);
+  
+   
+
+    
+   
+
+        
+
+    });
+ // when initialized, pass in info for first sample
+ let sample1 = sampleNames[0];
+
+ //call the function to build the metadata
+    demoInfo(sample1);
+
+//call function to build bar chart
+buildBarChart(sample1);
+
+//call function to build the bubble chart
+buildBubbleChart(sample1);
+});
+
+   
+}   
+
+    
+ 
+
+// function that updates the dashboard
+function optionChanged(item)
+{
+    //call the update to the metadata
+    demoInfo(item);
+    
+
+    //call function to build bar chart
+    buildBarChart(item);
+    
+    //call function to build the bubble chart
+    buildBubbleChart(item);
+
+}
+
+// call the initialize function
+initalize();
